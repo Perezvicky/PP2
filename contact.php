b54811e42ff971dd19171782c94837db738669b8
Tomas Guida <tomasguda2000@hotmail.com>
2022-08-23 18:04:06 -0300

    actualizado contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,15 @@
+<?php
+include_once('hms/include/config.php');
+if (isset($_POST['submit'])) {
+	$name = $_POST['fullname'];
+	$email = $_POST['emailid'];
+	$mobileno = $_POST['mobileno'];
+	$dscrption = $_POST['description'];
+	$query = mysqli_query($con, "insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+	echo "<script>alert('Su información fue enviada con éxito');</script>";
+	echo "<script>window.location.href ='contact.php'</script>";
+}
+?>
 <!DOCTYPE HTML>
 <html>
 
