c9c0715b0811ceeeb3bcbce86b557ff80336fe4b
Enzo <enzopac423@gmail.com>
2022-09-18 12:01:33 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/Perezvicky/PP2""
    This reverts commit 350a8bbb7ffc22564a04b00aa65305183e9790ae.

--- hms/user-login.php
+++ hms/user-login.php
@@ -43,6 +43,7 @@ exit();
 	<head>
 		<title>Inicio de sesión de usuario</title>
 		<link rel="icon" href="images/favicon.png">
+
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
