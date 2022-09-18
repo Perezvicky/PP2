350a8bbb7ffc22564a04b00aa65305183e9790ae
Enzo <enzopac423@gmail.com>
2022-09-18 12:01:28 -0300

    Revert "Merge branch 'main' of https://github.com/Perezvicky/PP2"
    This reverts commit bf1c24e37ef7590a780cee7860cf017b7e4e0754, reversing
    changes made to cbab59c058ebfb97c7f3345f40cc6099dfd9946b.

--- hms/forgot-password.php
+++ hms/forgot-password.php
@@ -25,19 +25,9 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-	<title>Pateint Recuperación de contraseña</title>
+	<title>Pateint Password Recovery</title>
 	<link rel="icon" href="images/favicon.png">
 
-	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
-	<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" href="assets/css/styles.css">
-	<link rel="stylesheet" href="assets/css/plugins.css">
-	<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 </head>
 
 <body class="login">
@@ -96,17 +86,7 @@ if (isset($_POST['submit'])) {
 
 		</div>
 	</div>
-	<script src="vendor/jquery/jquery.min.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="vendor/modernizr/modernizr.js"></script>
-	<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-	<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-	<script src="vendor/switchery/switchery.min.js"></script>
-	<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
 
-	<script src="assets/js/main.js"></script>
-
-	<script src="assets/js/login.js"></script>
 	<script>
 		jQuery(document).ready(function() {
 			Main.init();
